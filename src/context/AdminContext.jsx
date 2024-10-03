00d1de0bc72bf973d5fbea0f79b051dfb02d6694
George Ofogba <ofogbageorge3@gmail.com>
2024-10-03 23:14:31 +0100

    doctorslist added

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import { createContext, useState } from 'react';
+import { toast } from 'react-toastify';
 
 export const AdminContext = createContext();
 
@@ -6,10 +8,53 @@ const AdminContextProvider = (props) => {
   const [aToken, setAToken] = useState(
     localStorage.getItem('aToken') ? localStorage.getItem('aToken') : ''
   );
+  const [doctors, setDoctors] = useState([]);
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-  const value = { aToken, setAToken, backendUrl };
+  const getDoctors = async () => {
+    try {
+      const { data } = await axios.post(
+        `${backendUrl}/api/admin/doctors`,
+        {},
+        { headers: { aToken } }
+      );
+      if (data.success) {
+        setDoctors(data.data);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const changeAvailability = async (docId) => {
+    try {
+      const { data } = await axios.post(
+        `${backendUrl}/api/admin/change-availability`,
+        { docId },
+        { headers: { aToken } }
+      );
+      if (data.success) {
+        toast.success(data.message);
+        getDoctors();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const value = {
+    aToken,
+    setAToken,
+    backendUrl,
+    doctors,
+    getDoctors,
+    changeAvailability,
+  };
 
   return (
     <AdminContext.Provider value={value}>
